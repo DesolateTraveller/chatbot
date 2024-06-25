14af88d3b234c9843af0f9b6f6af7aeb1d090cfd
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-25 23:22:36 +0530

    Update chat_v0.1.py

--- chat_v0.1.py
+++ chat_v0.1.py
@@ -6,12 +6,28 @@ from streamlit_chat import message
 #---------------------------------------------------------------------------------------------------------------------------------
 ### Import Libraries
 #---------------------------------------------------------------------------------------------------------------------------------
+import os
 import tempfile  
-from langchain_community.document_loaders import CSVLoader 
-from langchain.embeddings import HuggingFaceEmbeddings 
-from langchain.vectorstores import FAISS
-from langchain.llms import CTransformers
-from langchain.chains import ConversationalRetrievalChain
+import asyncio
+#----------------------------------------
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from numpy import sqrt
+import matplotlib.pyplot as plt
+#----------------------------------------
+from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import CSVLoader
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain.chains.summarize import load_summarize_chain
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_experimental.agents import create_pandas_dataframe_agent
 #---------------------------------------------------------------------------------------------------------------------------------
 ### Title and description for your Streamlit app
 #---------------------------------------------------------------------------------------------------------------------------------
@@ -60,65 +76,183 @@ st.sidebar.divider()
 ### Functions & Definitions
 #---------------------------------------------------------------------------------------------------------------------------------
 
-DB_FAISS_PATH = 'vectorstore/db_faiss'
-def load_llm():
-    llm = CTransformers(model="llama-2-7b-chat.ggmlv3.q8_0.bin",
-                        model_type="llama",
-                        max_new_tokens=max_tokens,
-                        temperature=temperature)
-    return llm
+def home_page():
+    st.write("""Select any one feature from above sliderbox: \n
+    1. Chat with CSV \n
+    2. Summarize CSV \n
+    3. Analyze CSV  """)
 
-def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"] 
+@st.cache_resource()
+def retriever_func(uploaded_file):
+    if uploaded_file :
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file_path = tmp_file.name
+        try:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+            data = loader.load()
+        except:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="cp1252")
+            data = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+                        chunk_size=1000, 
+                        chunk_overlap=200, 
+                        add_start_index=True
+                        )
+        all_splits = text_splitter.split_documents(data)
+        vectorstore = FAISS.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+        retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+    if not uploaded_file:
+        st.info("Please upload CSV documents to continue.")
+        st.stop()
+    return retriever, vectorstore
+
+def chat(temperature, model_name):
+    st.write("# Talk to CSV")
+    # Add functionality for Page 1
+    reset = st.sidebar.button("Reset Chat")
+    uploaded_file = st.sidebar.file_uploader("Upload your CSV here 👇:", type="csv")
+    retriever, vectorstore = retriever_func(uploaded_file)
+    llm = ChatOpenAI(model_name=model_name, temperature=temperature, streaming=True)
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+    
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    store = {}
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """Use the following pieces of context to answer the question at the end.
+                  If you don't know the answer, just say that you don't know, don't try to make up an answer. Context: {context}""",
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
+        ]
+    )
+    runnable = prompt | llm
+    
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+
+    with_message_history = RunnableWithMessageHistory(
+        runnable,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+    async def chat_message():
+        if prompt := st.chat_input():
+            if not user_api_key: 
+                st.info("Please add your OpenAI API key to continue.")
+                st.stop()
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            contextt = vectorstore.similarity_search(prompt, k=6)
+            context = "\n\n".join(doc.page_content for doc in contextt)
+            #msg = 
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                text_chunk = ""
+                async for chunk in with_message_history.astream(
+                        {"context": context, "input": prompt},
+                        config={"configurable": {"session_id": "abc123"}},
+                    ):
+                    text_chunk += chunk.content
+                    message_placeholder.markdown(text_chunk)
+                    #st.chat_message("assistant").write(text_chunk)
+                st.session_state.messages.append({"role": "assistant", "content": text_chunk})
+        if reset:
+            st.session_state["messages"] = []
+    asyncio.run(chat_message())
+
+
+def summary(model_name, temperature, top_p):
+    st.write("# Summary of CSV")
+    st.write("Upload your document here:")
+    uploaded_file = st.file_uploader("Upload source document", type="csv", label_visibility="collapsed")
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file_path = tmp_file.name
+        # encoding = cp1252
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1024, chunk_overlap=100)
+        try:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="cp1252")
+            #loader = UnstructuredFileLoader(tmp_file_path)
+            data = loader.load()
+            texts = text_splitter.split_documents(data)
+        except:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+            #loader = UnstructuredFileLoader(tmp_file_path)
+            data = loader.load()
+            texts = text_splitter.split_documents(data)
+
+        os.remove(tmp_file_path)
+        gen_sum = st.button("Generate Summary")
+        if gen_sum:
+            # Initialize the OpenAI module, load and run the summarize chain
+            llm = ChatOpenAI(model_name=model_name, temperature=temperature)
+            chain = load_summarize_chain(
+                llm=llm,
+                chain_type="map_reduce",
+
+                return_intermediate_steps=True,
+                input_key="input_documents",
+                output_key="output_text",
+            )
+            result = chain({"input_documents": texts}, return_only_outputs=True)
+
+            st.success(result["output_text"])
+
+
+def analyze(temperature, model_name):
+    st.write("# Analyze CSV")
+    #st.write("This is Page 3")
+    # Add functionality for Page 3
+    reset = st.sidebar.button("Reset Chat")
+    uploaded_file = st.sidebar.file_uploader("Upload your CSV here 👇:", type="csv")
+    #.write(uploaded_file.name)
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file_path = tmp_file.name
+        df = pd.read_csv(tmp_file_path)
+        llm = ChatOpenAI(model=model_name, temperature=temperature)
+        agent = create_pandas_dataframe_agent(llm, df, agent_type="openai-tools", verbose=True)
+
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+            
+        for msg in st.session_state.messages:
+            st.chat_message(msg["role"]).write(msg["content"])
+
+        if prompt := st.chat_input(placeholder="What are the names of the columns?"):
+            if not user_api_key: 
+                st.info("Please add your OpenAI API key to continue.")
+                st.stop()
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            msg = agent.invoke({"input": prompt, "chat_history": st.session_state.messages})
+            st.session_state.messages.append({"role": "assistant", "content": msg["output"]})
+            st.chat_message("assistant").write(msg["output"])
+        if reset:
+            st.session_state["messages"] = []
 
 #---------------------------------------------------------------------------------------------------------------------------------
 ### Main App
 #---------------------------------------------------------------------------------------------------------------------------------
 
-uploaded_file = st.sidebar.file_uploader("Upload File", type="csv") 
-if uploaded_file:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name # save file locally
-
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={'delimiter': ','}) 
-    data = loader.load()
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs={'device': 'cpu'}) 
-
-    db = FAISS.from_documents(data, embeddings) 
-    db.save_local(DB_FAISS_PATH) 
-    llm = load_llm() 
-
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
-
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
-
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me about " + uploaded_file.name + " 🤗"]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
-
-    response_container = st.container() # form
-    container = st.container()
-
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Query:", placeholder="Talk to csv data 👉 (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+user_api_key = st.sidebar.text_input(label="#### Enter OpenAI API key 👇", placeholder="Paste your openAI API key, sk-", type="password", key="openai_api_key")
+if user_api_key:
+    st.sidebar.success("API key loaded", icon="🚀")
